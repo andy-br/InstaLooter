a55e9300debf77e768653e0acf71997c3543ce39
andy-br <andrew.rigby@brconsult.co.uk>
2018-12-04 00:15:22 +0000

    Update pages.py

--- instalooter/pages.py
+++ instalooter/pages.py
@@ -34,7 +34,7 @@ class PageIterator(typing.Iterator[typing.Dict[typing.Text, typing.Any]]):
     """
 
     PAGE_SIZE = 50
-    INTERVAL = 40
+    INTERVAL = 30
 
     _BASE_URL = "https://www.instagram.com/graphql/query/"
     _section_generic = NotImplemented    # type: Text
